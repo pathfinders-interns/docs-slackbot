af34ee8bbb71435b3fa56174ff312910acd503e9
Law111 <ljoa30@stuy.edu>
2022-08-24 14:44:11 -0400

    removed unnecessary code

--- git_test.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-subprocess.call(['git', 'add', 'test.md'], cwd="../../docs-slackbot.wiki")
-subprocess.call(['git', 'commit', '-am', "commit"], cwd="../../docs-slackbot.wiki")
-subprocess.call(['git', 'push'], cwd="../../docs-slackbot.wiki")
